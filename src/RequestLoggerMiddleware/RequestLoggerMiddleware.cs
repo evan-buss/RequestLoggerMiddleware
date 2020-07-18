99fa8588ce3791c844ee7c999165a603362c823d
Evan Buss <evan.buss28@gmail.com>
2020-07-18 13:48:45 -0400

    Project setup and basic implementation

---
+++ src/RequestLoggerMiddleware/RequestLoggerMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RequestLoggerMiddleware
+{
+    public class RequestLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            await _next(context).ConfigureAwait(false);
+
+            stopwatch.Stop();
+            Console.WriteLine($"{context.Request.Path} -- {stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
---
+++ src/RequestLoggerMiddleware/RequestLoggerMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace RequestLoggerMiddleware
+{
+    public static class RequestLoggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLogger(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggerMiddleware>();
+        }
+    }
+}
